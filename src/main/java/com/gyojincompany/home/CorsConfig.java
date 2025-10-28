ca66a9bbb04146165eec563fd6d6e88619bdbf9f
gyojincompany <luckyred@gmail.com>
2025-10-28 09:39:39 +0900

    Cors 설정 파일 CorsConfig 추가

---
+++ src/main/java/com/gyojincompany/home/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.gyojincompany.home;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")           // 모든 경로 허용
+                        .allowedOrigins("http://localhost:3000","http://localhost") // 허용할 origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
